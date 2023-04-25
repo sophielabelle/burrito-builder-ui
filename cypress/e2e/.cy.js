dfec677f60931f7bd48e809c767efd9acec65411
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-04-25 10:08:37 -0700

    Add fixtures data

---
+++ cypress/e2e/.cy.js
@@ -0,0 +1,9 @@
+// As a user when I first come to the page I should see any orders that already exist
+// As a user I should be able to make a new order by adding my name and any ingredients I want on my order
+// As a user I should be told if my order is not put through for some reason
+
+describe('Main page', () => {
+  it('Should display a form, orders and ', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
